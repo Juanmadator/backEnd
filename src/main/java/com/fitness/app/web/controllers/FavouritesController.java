788c5dfbe3d5828de249448972b76777626c2b53
Juanma <juanmadametor@gmail.com>
2024-05-17 18:30:33 +0200

    Cambiado el Dockerfile

--- src/main/java/com/fitness/app/web/controllers/FavouritesController.java
+++ src/main/java/com/fitness/app/web/controllers/FavouritesController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -42,5 +39,11 @@ private final FavouriteRepository favouriteRepository;
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @GetMapping("/mishuevos")
+    public String saludo(){
+
+        return "mis huevos";
     }
 }
